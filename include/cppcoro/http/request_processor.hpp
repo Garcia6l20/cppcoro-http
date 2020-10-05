5eeee6aef04b4fd7bbe1c86038276815d3a9a1b8
Sylvain Garcia <garcia.6l20@gmail.com>
2020-10-06 01:25:35 +0200

    fix lambda capture
    - still some strange lambda capture bugs on GCC (10 and 11)
      should avoid it from now

--- include/cppcoro/http/request_processor.hpp
+++ include/cppcoro/http/request_processor.hpp
@@ -17,39 +17,39 @@ namespace cppcoro::http {
         using server::server;
 
         task<> serve() {
-            auto handle_conn = [this](http::server::connection_type conn) mutable -> task<> {
-                session_type session{};
-                http::string_request default_request;
-                auto init_request = [&](const http::request_parser &parser) -> http::detail::base_request& {
-                    auto *request = static_cast<ProcessorT*>(this)->prepare(parser, session);
-                    if (!request) {
-                        return default_request;
-                    }
-                    return *request;
-                };
-                while (true) {
-                    try {
-                        // wait next connection request
-                        auto req = co_await conn.next(init_request);
-                        if (!req)
-                            break; // connection closed
-                        // process and send the response
-                        co_await conn.send(co_await static_cast<ProcessorT*>(this)->process(*req));
-                    } catch (std::system_error &err) {
-                        if (err.code() == std::errc::connection_reset) {
-                            break; // connection reset by peer
-                        } else {
-                            throw err;
-                        }
-                    } catch (operation_cancelled &) {
-                        break;
-                    }
-                }
-            };
             async_scope scope;
             try {
                 while (true) {
-                    scope.spawn(handle_conn(co_await listen()));
+                    auto conn = co_await listen();
+                    scope.spawn([](server *srv, http::server::connection_type conn) mutable -> task<> {
+                        session_type session{};
+                        http::string_request default_request;
+                        auto init_request = [&](const http::request_parser &parser) -> http::detail::base_request& {
+                            auto *request = static_cast<ProcessorT*>(srv)->prepare(parser, session);
+                            if (!request) {
+                                return default_request;
+                            }
+                            return *request;
+                        };
+                        while (true) {
+                            try {
+                                // wait next connection request
+                                auto req = co_await conn.next(init_request);
+                                if (!req)
+                                    break; // connection closed
+                                // process and send the response
+                                co_await conn.send(co_await static_cast<ProcessorT*>(srv)->process(*req));
+                            } catch (std::system_error &err) {
+                                if (err.code() == std::errc::connection_reset) {
+                                    break; // connection reset by peer
+                                } else {
+                                    throw err;
+                                }
+                            } catch (operation_cancelled &) {
+                                break;
+                            }
+                        }
+                    }(this, std::move(conn)));
                 }
             } catch (operation_cancelled &) {}
             co_await scope.join();
